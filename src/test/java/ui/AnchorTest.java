cc657aec2b7309a4eb7e5ce18d3674b52ba9ad77
Jeong Seon U <jung48273@gmail.com>
2022-06-07 18:35:25 +0900

    /** * Purpose: Check copy instance * Input: copy * Expected: * 	return same value of anchor properties */

---
+++ src/test/java/ui/AnchorTest.java
@@ -0,0 +1,47 @@
+package ui;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import project_16x16.SideScroller;
+import project_16x16.ui.Anchor;
+
+public class AnchorTest {
+
+	private Anchor anchor;
+	
+	@Before
+	public void setUp() throws Exception {
+		SideScroller sideScroller = new SideScroller(); 
+		anchor = new Anchor(sideScroller,10, 10, 10, 10);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		anchor=null;
+	}
+	
+	/**
+	 * Purpose: Check copy instance
+	 * Input: copy 
+	 * Expected:
+	 * 	return same value of anchor properties
+	 */
+	@Test
+	public void TestCopy() {
+		Anchor tmp = anchor.copy();
+		assertNotNull(tmp);
+		assertEquals(tmp.getLocalHeight(), anchor.getLocalHeight());
+		assertEquals(tmp.getLocalWidth(), anchor.getLocalWidth());
+		assertEquals(tmp.getLocalX(), anchor.getLocalX());
+		assertEquals(tmp.getLocalY(), anchor.getLocalY());
+		assertEquals(tmp.getPApplet(), anchor.getPApplet());
+	}
+
+	
+}
