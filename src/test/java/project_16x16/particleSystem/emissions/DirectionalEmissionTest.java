3f72e05d09ce7e77d20a53bf3d1a10e016380ab4
DarkhM01 <49951716+DarkhM01@users.noreply.github.com>
2022-06-08 16:58:52 +0900

    Update DirectionalEmissionTest.java
    	/**
    	 * Purpose: test for Construct() method
    	 * Input: (vector, 0, 1, 2, 3)
    	 * Expected:
    	 * 		return DirectionalEmission object with given values
    	 */

--- src/test/java/project_16x16/particleSystem/emissions/DirectionalEmissionTest.java
+++ src/test/java/project_16x16/particleSystem/emissions/DirectionalEmissionTest.java
@@ -26,4 +26,20 @@ class DirectionalEmissionTest {
 		assertEquals(copy.getAngle(), direct.getAngle());
 	}
 
+	/**
+	 * Purpose: test for Construct() method
+	 * Input: (vector, 0, 1, 2, 3)
+	 * Expected:
+	 * 		return DirectionalEmission object with given values
+	 */
+	@Test
+	void testDirectionalEmission() {
+		PVector vector = new PVector(0, 0);
+		DirectionalEmission direct = new DirectionalEmission(vector, 0, 1, 2, 3);
+		assertNotNull(direct);
+		assertEquals(0, direct.getVelocity());
+		assertEquals(1, direct.getAcceleration());
+		assertEquals(2, direct.getSpread());
+		assertEquals(3, direct.getAngle());
+	}
 }
