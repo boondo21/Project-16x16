74b5b4f063ef1b78fe30b69ce87fc3cd206ca74a
DarkhM01 <49951716+DarkhM01@users.noreply.github.com>
2022-06-08 20:24:20 +0900

    Update ParticleAccelerationControllerTest.java
    	/**
    	 * Purpose: Test for onParticleSpawnEvent()
    	 * Input: Particle p with velocity set to PVector(1, 1)
    	 * Expected:
    	 * 		new acceleration should be equal to the sum of original and new accelerations
    	 */

--- src/test/java/project_16x16/particleSystem/events/ParticleAccelerationControllerTest.java
+++ src/test/java/project_16x16/particleSystem/events/ParticleAccelerationControllerTest.java
@@ -23,4 +23,22 @@ class ParticleAccelerationControllerTest {
 		ParticleAccelerationController controller = new ParticleAccelerationController(new PVector(0, 0));
 		assertEquals(controller.getAcceleration(), new PVector(0, 0));
 	}
+
+	/**
+	 * Purpose: Test for onParticleSpawnEvent() 
+	 * Input: Particle p with velocity set to PVector(1, 1)
+	 * Expected:
+	 * 		new acceleration should be equal to the sum of original and new accelerations
+	 */
+	@Test
+	void testOnParticleSpawnEvent() {
+		PVector acceleration = new PVector(0, 0);
+		ParticleAccelerationController controller = new ParticleAccelerationController(acceleration);
+		SideScroller applet = new SideScroller();
+		PImage img = new PImage();
+		Particle p = new Particle(applet, img);
+		p.setAcceleration(new PVector(1, 1));
+		controller.onParticleSpawnEvent(p);
+		assertEquals(p.getAcceleration(), acceleration.add(new PVector(1, 1)));
+	}
 }
