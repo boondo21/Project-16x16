38de48d29195474342bfacf0e300934e77374a8b
DarkhM01 <49951716+DarkhM01@users.noreply.github.com>
2022-06-08 19:41:19 +0900

    Update ParticlePreloadSystemTest.java
    /**
    	 * Purpose: Test for preload()
    	 * Input: 1 - number of frames
    	 * Expected:
    	 * 		Consumer<Particle> object
    	 * 		call spawn to create a particle for consumer and make it accept it
    	 */

--- src/test/java/project_16x16/particleSystem/ParticlePreloadSystemTest.java
+++ src/test/java/project_16x16/particleSystem/ParticlePreloadSystemTest.java
@@ -6,16 +6,29 @@ import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 
+import processing.core.PImage;
+import processing.core.PVector;
+import project_16x16.SideScroller;
+
 class ParticlePreloadSystemTest {
 	/**
 	 * Purpose: Test for preload() 
-	 * Input: 60 - number of frames
+	 * Input: 1 - number of frames
 	 * Expected:
 	 * 		Consumer<Particle> object
+	 * 		call spawn to create a particle for consumer and make it accept it
 	 */
 	@Test
 	void testPreload() {
-		Consumer<Particle> c = ParticlePreloadSystem.preload(60);
+		SideScroller applet = new SideScroller();
+		PImage img = new PImage();
+		Consumer<Particle> c = ParticlePreloadSystem.preload(1);
+		Particle p = new Particle(applet, img);
+		p.spawn(c, 100);
+		p.setPosition(new PVector(1, 1));
+		p.setVelocity(new PVector(1, 1));
+		p.setAcceleration(new PVector(1, 1));
+		p.spawn(c, 100);
 		assertNotNull(c);
 	}
 
