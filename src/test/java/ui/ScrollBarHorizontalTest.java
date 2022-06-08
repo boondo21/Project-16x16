fcfc400513e1ff80258a500603fd135442911627
Jeong Seon U <jung48273@gmail.com>
2022-06-08 21:13:16 +0900

    /** * Purpose: Check setAnchor * Input: setAnchor * Expected: * 	return void */

---
+++ src/test/java/ui/ScrollBarHorizontalTest.java
@@ -0,0 +1,43 @@
+package ui;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import project_16x16.SideScroller;
+import project_16x16.ui.Anchor;
+import project_16x16.ui.Notifications;
+import project_16x16.ui.ScrollBarHorizontal;
+
+public class ScrollBarHorizontalTest {
+	
+	ScrollBarHorizontal scrollBarHorizontal;
+	Anchor anchor;
+	@Before
+	public void setUp() throws Exception {
+		anchor = new Anchor(new SideScroller(), 10, 10, 10, 10);
+		scrollBarHorizontal = new ScrollBarHorizontal(anchor);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		anchor=null;
+		scrollBarHorizontal=null;
+	}
+	
+	/**
+	 * Purpose: Check setAnchor
+	 * Input: setAnchor 
+	 * Expected:
+	 * 	return void
+	 */
+	@Test
+	public void testsetAnchor() {
+		try {
+			Anchor anc = new Anchor(new SideScroller(), 20, 20, 20, 20);
+			scrollBarHorizontal.setAnchor(anc);
+		} catch (Exception e) {
+			System.out.print(e.getMessage());
+		}
+	}
+}
