0f4a2b0a4c13f2305cac833e383a8687ff7b92f3
DarkhM01 <49951716+DarkhM01@users.noreply.github.com>
2022-06-08 23:27:41 +0900

    Update MultiplayerTest.java
    	/**
    	 * Purpose: Test for Constructor() for client
    	 * Input: SideScroller s, hostIP - "127.0.0.1", port - 25563, isHost - false
    	 * Expected:
    	 * 		Client should be active
    	 */

--- src/test/java/project_16x16/multiplayer/MultiplayerTest.java
+++ src/test/java/project_16x16/multiplayer/MultiplayerTest.java
@@ -23,4 +23,21 @@ class MultiplayerTest {
 		assertTrue(m.getServer().active());
 		m.exit();
 	}
+	
+	/**
+	 * Purpose: Test for Constructor() for client
+	 * Input: SideScroller s, hostIP - "127.0.0.1", port - 25563, isHost - false
+	 * Expected:
+	 * 		Client should be active
+	 */
+	@Test
+	void testMultiplayerSideScrollerStringIntBoolean3() throws ConnectException {
+		SideScroller s = new SideScroller();
+
+		Multiplayer m1 = new Multiplayer(s, "127.0.0.1", 25563, true);
+		Multiplayer m2 = new Multiplayer(s, "127.0.0.1", 25563, false);
+		assertTrue(m2.getClient().active());
+		m2.exit();
+		m1.exit();
+	}
 }
