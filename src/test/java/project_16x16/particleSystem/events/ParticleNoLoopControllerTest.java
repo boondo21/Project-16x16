7cb725f48e7399e311f28a044350b75e0f767e5e
DarkhM01 <49951716+DarkhM01@users.noreply.github.com>
2022-06-08 20:47:09 +0900

    Update ParticleNoLoopControllerTest.java
    	/**
    	 * Purpose: Test for onCreateEvent()
    	 * Input: ParticleSystem system
    	 * Expected:
    	 * 		controller has the system given to him
    	 */

--- src/test/java/project_16x16/particleSystem/events/ParticleNoLoopControllerTest.java
+++ src/test/java/project_16x16/particleSystem/events/ParticleNoLoopControllerTest.java
@@ -23,4 +23,20 @@ class ParticleNoLoopControllerTest {
 		assertEquals(controller.getSpawnAmount(), 20);
 		assertEquals(controller.getTotalSpawned(), 0);
 	}
+
+	/**
+	 * Purpose: Test for onCreateEvent() 
+	 * Input: ParticleSystem system
+	 * Expected:
+	 * 		controller has the system given to him
+	 */
+	@Test
+	void testOnCreateEvent() {
+		SideScroller applet = new SideScroller();
+		PImage img = new PImage();
+		ParticleSystem system = new ParticleSystem(applet, img, 0, 0, 0);
+		ParticleNoLoopController controller = new ParticleNoLoopController(20);
+		controller.onCreateEvent(system);
+		assertSame(controller.getParticleSystem(), system);		
+	}
 }
